fd1a0b60ff2b9c7f268d6f8885bd8e016779d4a9
itsremor <akamorganz@gmail.com>
2020-09-17 17:15:42 +0300

    First commit

---
+++ src/DrawPanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D gr = (Graphics2D) g;
+
+    }
+
+    //запускается в методе paint, чтобы работало
+}
+
